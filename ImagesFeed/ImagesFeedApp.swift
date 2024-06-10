13fac04a39a3c9f3301b30bab8e96bef08fc58be
IlyaBSyazan <bachkou.i@syazan.com>
2024-06-10 15:53:49 +0200

    Initial Commit

---
+++ ImagesFeed/ImagesFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesFeedApp.swift
+//  ImagesFeed
+//
+//  Created by Илья Бочков on 10.06.24.
+//
+
+import SwiftUI
+
+@main
+struct ImagesFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
